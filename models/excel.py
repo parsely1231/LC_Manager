0cf0f01ac91d469b8ec4528db4523550008aedc5
parsely <parsely_1231@icloud.com>
2020-03-12 22:59:12 +0900

    implement excluded function
    1. views: create popup.py file and ExcludePopup class
    2. presenter: With '-Exclude-' event, the set(excluded) is got from ExcludePopup, and start model.set_exclude
    3. add 'function complete' popup and the function updating table in view to all presenter function
    4. ExcelModel: if peak is excluded, the cells filled gray color
    5. ImpurityData Model: if peak is excluded, the impdata.edited_area_ratio be to None.

--- models/excel.py
+++ models/excel.py
@@ -2,6 +2,7 @@ from models.hplc_data import ExperimentalData, DataTable, ImpurityData
 
 
 import openpyxl as excel
+from openpyxl.styles import PatternFill
 
 
 class ExcelModel:
@@ -23,15 +24,14 @@ class ExcelModel:
         x = 2
         y = 2
         for _, table in exp.tables.items():
-            self.write_table(table, y, x)
+            self.write_table(table, y, x, exp.imp_excluded)
             x += self.TABLE_SIZE
 
         self.write_imp_trend(exp)
         self.wb.save(file_path)
         self.wb.close()
 
-
-    def write_table(self, table: DataTable, y, x):
+    def write_table(self, table: DataTable, y, x, excluded):
         """worksheetにtable一つ分のデータを書き込む"""
 
         self.ws.cell(y, x, table.name)  # table.nameを初期位置に書く
@@ -39,16 +39,20 @@ class ExcelModel:
             nx = x + dx
             self.ws.cell(y+1, nx, element)
 
+        fill = PatternFill(fill_type='solid', fgColor='d3d3d3')
         for dy, imp_data in enumerate(table.data_list, 2):  # 各不純物のデータを書く
             self.write_imp_data(imp_data, y+dy, x)
+            if imp_data.name in excluded:
+                for col in range(x, x+self.TABLE_SIZE-1):
+                    self.ws.cell(y+dy, col).fill = fill
 
         final_row = y+2+len(table.data_list)
+        self.ws.cell(final_row, x + self.ELEMENT_POS['Area']-1, 'Total Area')
+        self.ws.cell(final_row + 1, x + self.ELEMENT_POS['Area']-1, 'Excluded')
         self.ws.cell(final_row, x+self.ELEMENT_POS['Area'], table.total_area)
-        self.ws.cell(final_row, x+self.ELEMENT_POS['補正Area%'], table.edited_total_area)
+        self.ws.cell(final_row+1, x+self.ELEMENT_POS['Area'], table.edited_total_area)
 
     def write_imp_data(self, data: ImpurityData, y, x):
-        """worksheetにimp一つ分のデータを書き込む
-        TODO excludedピークの場合、セルに色をつける機能を追加"""
         self.ws.cell(y, x+self.ELEMENT_POS['name'], data.name)
         self.ws.cell(y, x+self.ELEMENT_POS['RT'], data.rt)
         self.ws.cell(y, x+self.ELEMENT_POS['RRT'], data.rrt)
--- views/popup.py
+++ views/popup.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 class NamePeakPopup:
     def __init__(self, rrt_list):
         cols = [[sg.Text('RRT', size=(5, 1)), sg.Text('Name', size=(15, 1))]] + \
-              [[sg.Text(str(rrt), size=(5, 1)), sg.Input(size=(15, 1))] for rrt in rrt_list]
+               [[sg.Text(str(rrt), size=(5, 1)), sg.Input(size=(15, 1))] for rrt in rrt_list]
 
         self.layout = [[sg.Column(cols,scrollable=True , vertical_scroll_only=True, size=(200, 400))],
                        [sg.OK(), sg.Cancel()]]
@@ -28,3 +28,31 @@ class NamePeakPopup:
 
         self.window.close()
         return self.rrt_to_name
+
+
+class ExcludePopup:
+    def __init__(self, imp_name_list):
+        self.layout = [[sg.Text('Check Exclude Peak', size=(20, 1))]] + \
+                 [[sg.Checkbox(name)] for name in imp_name_list if name is not None] + \
+                 [[sg.OK(), sg.Cancel()]]
+
+        self.window = sg.Window(title='Set Exclude', layout=self.layout)
+        self.excluded = set()
+        self.imp_name_list = imp_name_list
+
+    def get_excluded(self):
+        while True:
+            event, values = self.window.read()
+
+            if event in ('Exit', 'Quit', 'Cancel', None):
+                break
+
+            elif event == 'OK':
+                for name, check in zip(self.imp_name_list, values.values()):
+                    if check:
+                        self.excluded.add(name)
+                break
+
+        self.window.close()
+        return self.excluded
+
