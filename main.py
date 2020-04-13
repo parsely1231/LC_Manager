54e5d591c8f45aa78f262efd78c00f13b67d37bd
parsely <parsely_1231@icloud.com>
2020-04-13 17:28:35 +0900

    イベントの振り分けをhandlerで実装

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def main():
     from presenter.presenter import Presenter
+    from presenter.handler import Handler
     from views.view import InterFace
 
     interface = InterFace()
     interface.show_window()
     presenter = Presenter(window=interface.window)
+    handler = Handler(presenter)
 
     while True:
         event, values = interface.window.read()
-        presenter.check_event(event, values)
+        handler.handle(event, values)
 
-        if event in ('Exit', 'Quit', None):
+        if event is None:
             break
 
     interface.close_window()
